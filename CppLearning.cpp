44d8ab1a39bddf7895cef52147f1e3769e631841
Phil - DopeDealers <phillip.kinney@cyci.org>
2024-05-15 20:38:53 -0400

    Add project files.

---
+++ CppLearning.cpp
@@ -0,0 +1,13 @@
+﻿// CppLearning.cpp : Defines the entry point for the application.
+//
+
+#include "CppLearning.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	return 0;
+}
